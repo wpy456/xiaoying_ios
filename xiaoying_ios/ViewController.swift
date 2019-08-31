0d6ac5d5df00e06578cc6a28002365ab1b9d80e8
wpy456 <wpy456@sohu.com>
2019-08-31 22:04:27 +0800

    Initial Commit

---
+++ xiaoying_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xiaoying_ios
+//
+//  Created by PeiYu Wang on 2019/8/31.
+//  Copyright © 2019 PeiYu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
