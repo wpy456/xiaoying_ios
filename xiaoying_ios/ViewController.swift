aea8dbcbd794f4e036ae9c1ac9be68f0120bd7c5
wpy456 <wpy456@sohu.com>
2019-09-04 22:29:18 +0800

    0

--- xiaoying_ios/ViewController.swift
+++ xiaoying_ios/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
-
+import CryptoSwift
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do {
+            let str = "欢迎访问 hangge.com"
+            print("原始字符串：\(str)")
+            
+            let key = "hangge.com123456"
+            print("key密钥：\(key)")
+            
+            let iv = "1234567890123456"
+            print("密钥偏移量：\(iv)")
+            
+            //使用AES-128-CBC加密模式
+            let aes = try AES(key: key.bytes, blockMode: CBC(iv: iv.bytes))
+            
+            //开始加密
+            let encrypted = try aes.encrypt(str.bytes)
+            print("加密结果(base64)：\(encrypted.toBase64()!)")
+            
+            //开始解密
+            let decrypted = try aes.decrypt(encrypted)
+            print("解密结果：\(String(data: Data(decrypted), encoding: .utf8)!)")
+            print("md结果：\(String(data: Data(decrypted), encoding: .utf8)!.md5())")
+        } catch { }
     }
 
 
